f35c07746d546e5d896f22ed3bc9289aa60f712c
Sahil Shakya <sahilshakya622@gmail.com>
2026-01-19 16:42:10 +0530

    Fix: add PostgreSQL version

--- terraform/database.tf
+++ terraform/database.tf
@@ -2,7 +2,10 @@ resource "azurerm_postgresql_flexible_server" "db" {
   name                   = "cloud-db-server"
   location               = var.location
   resource_group_name    = azurerm_resource_group.rg.name
+
   administrator_login    = "dbadmin"
   administrator_password = "Password@123"
-  sku_name               = "B_Standard_B1ms"
+
+  sku_name = "B_Standard_B1ms"
+  version  = "14"
 }
